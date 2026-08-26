acd31e8a9751a60f922ee9ac7fee959bec85b3c7
aber0724 <69146982+aber0724@users.noreply.github.com>
2026-08-27 04:48:50 +0900

    fix(kitty): use platform-specific font sizes

--- nbshell/install.sh
+++ nbshell/install.sh
@@ -155,6 +155,8 @@ fi
 printf '\n== Configure Kitty terminal ==\n'
 mkdir -p "$CONFIG_HOME/kitty" "$CONFIG_HOME/environment.d"
 ln -sfnT "$SCRIPT_DIR/../kitty/kitty.conf" "$CONFIG_HOME/kitty/kitty.conf"
+ln -sfnT "$SCRIPT_DIR/../kitty/platform-linux.conf" "$CONFIG_HOME/kitty/platform-linux.conf"
+ln -sfnT "$SCRIPT_DIR/../kitty/platform-macos.conf" "$CONFIG_HOME/kitty/platform-macos.conf"
 cat > "$CONFIG_HOME/environment.d/50-terminal.conf" <<'EOF'
 TERMINAL=kitty
 EOF
